41b6c02485a6fbda1fbcaf398021d1967ad0422c
NaveenPrakash-N <naveenprakash2003n@gmail.com>
2026-07-15 15:06:36 +0530

    reverse_txt added

---
+++ reverse_txt.py
@@ -0,0 +1,15 @@
+text = "Reverse this message!"
+op = ''
+reversed = text[::-1]
+for i in range(len(reversed)):
+    if reversed[i].isalpha():
+        if i == 0:
+            op += reversed[i].upper()
+        elif reversed[i-1] == ' ':
+            op += reversed[i].upper()
+        else:
+            op += reversed[i].lower()
+    else:
+            op += reversed[i]  
+print(op)
+print(reversed)
